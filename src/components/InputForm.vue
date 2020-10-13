f432df4d9275232357924d8d3478c5dfe46b29cc
Meyerka <40023031+Meyerka@users.noreply.github.com>
2020-10-13 17:32:58 +0200

    data model for employers

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -2,10 +2,23 @@
   <div>
     <v-container>
       <h1>DPAE Sender</h1>
+
+      <h2>Contract</h2>
       <v-text-field v-model="siretNumber" label="SIRET Number"></v-text-field>
-      <input type="text" v-model="urssafCode" />
-      <input type="text" v-model="nafCode" />
-      <input type="text" v-model="employerName" />
+      <v-text-field v-model="urssafCode" label="URSSAF Code"></v-text-field>
+      <v-text-field v-model="nafCode" label="NAF Code"></v-text-field>
+
+      <h2>Employer 1</h2>
+      <v-text-field v-model="employer[0].name" label="Employer name"></v-text-field>
+      <v-text-field v-model="employer[0].postCode" label="Employer postcode"></v-text-field>
+      <v-text-field v-model="employer[0].city" label="Employer city"></v-text-field>
+
+      <h2>Employer 2</h2>
+      <v-text-field v-model="employer[1].name" label="Employer name"></v-text-field>
+      <v-text-field v-model="employer[1].postCode" label="Employer postcode"></v-text-field>
+      <v-text-field v-model="employer[1].city" label="Employer city"></v-text-field>
+
+      <h1>{{employer[1].postCode}}</h1>
     </v-container>
   </div>
 </template>
@@ -16,6 +29,18 @@ export default {
   data: () => ({
     siretNumber: "",
     urssafCode: "",
+    employer: [
+      {
+        name: "",
+        postCode: "",
+        city: "",
+      },
+      {
+        name: "",
+        postCode: "",
+        city: "",
+      },
+    ],
   }),
   props: {},
 };
