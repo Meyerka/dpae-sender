7bcf5f953e4f1dd1177f352c9e11abbd210dc7b0
Meyerka <40023031+Meyerka@users.noreply.github.com>
2020-10-19 18:44:02 +0200

    added xml preview button

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <v-app-bar app color="success" dark>
       <v-toolbar-title>DPAE Sender</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn color="grey darken-3" class="my-6">XML Preview</v-btn>
     </v-app-bar>
     <v-main>
       <InputForm />
