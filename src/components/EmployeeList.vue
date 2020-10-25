c97719661ca047a59772bfde68388f6f8aa711fa
Meyerka <40023031+Meyerka@users.noreply.github.com>
2020-10-25 10:06:15 +0100

    created employee list table

---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="employee in $store.state.employees" :key="employee.name">
+            <td>{{ employee.lastName }} {{employee.firstName}}</td>
+            <td>{{ employee.gender }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
